1beba11900073d07ed7c76dff68ac85f7b26a59b
ProgLCS <lucascorreacardoso@hotmail.com>
2025-04-24 10:43:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,22 @@
 document.getElementById('loginForm').addEventListener('submit', function(e) {
   e.preventDefault();
   
- // verificação
+ 
   const username = document.getElementById('username').value;
   const password = document.getElementById('password').value;
   
-  // Aqui você adicionaria a lógica real de verificação
-  // Esta é apenas uma simulação para mostrar a mensagem de erro
-  const isValid = false; // Alterar para true para simular login bem-sucedido
+ // Como não se tem os parâmetros próprios, deixei essa como exemplo para validar  nome e senha
+  const isValid = false; 
   
   if (!isValid) {
       document.getElementById('errorMessage').style.display = 'block';
   } else {
-      // Redirecionar ou mostrar conteúdo após login bem-sucedido
+      
       alert('Login bem-sucedido!');
       document.getElementById('errorMessage').style.display = 'none';
   }
 });
 
 document.getElementById('createAccount').addEventListener('click', function() {
-  // Redirecionar para a página de criação de conta
-  alert('Redirecionando para a página de criação de conta...');
+  window.location.href = 'Cadastro.html'; 
 });
